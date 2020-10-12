c3f26e453a192097f877b8a846364e7935854e5e
wRosie <43913069+wRosie@users.noreply.github.com>
2020-10-12 18:03:03 -0500

    Implemented findMagic.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# SQL_Magiclight
+# notebook_Magiclight
 ![GitHub](https://img.shields.io/github/license/wRosie/SQL_Magiclight?label=license)
 [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
 ![GitHub last commit](https://img.shields.io/github/last-commit/wRosie/SQL_Magiclight)
 
-This is a Jupyter notebook extension to add syntax highlighting for SQL codes in cell magic. 
+This is a Jupyter notebook extension to add availible syntax highlighting for cell magic. 
 
 It is currently under development. The first release will come soon.
 
--- index.js
+++ index.js
@@ -5,48 +5,49 @@ define([
     'codemirror/addon/mode/loadmode'
 ], function (Jupyter, events, codecell, CodeMirror) {
 
-    // var removeHighlight = function(){
-    //     // Jupyter.notebook.get_selected_cell().element.children()[0].getElementsByClassName('inner_cell')[0].getElementsByClassName('input_area')[0]
-    //     console.log("in remove");
-    //     var cell = Jupyter.notebook.get_selected_cell();
-
-    //     // var script = document.createElement('script');
-    //     // script.onload = function () {
-    //     //     console.log("loaded");
-    //     //     CodeMirror.autoLoadMode(cell.code_mirror, 'sql');
-    //     // };
-    //     // script.src = '/usr/local/share/jupyter/nbextensions/SQL_Magiclight/codemirror/addon/mode/loadmode.js';
-    //     // document.head.appendChild(script); //or something of the likes
-    //     //CodeMirror.autoLoadMode(cell.code_mirror, 'sql');
-    //     CodeMirror.modeURL = "codemirror/mode/sql/sql.js";
-    //     if (cell instanceof codecell.CodeCell){
-    //         console.log("in set");
-    //         //cell.code_mirror.setOption('mode','null');
-    //         cell.force_highlight('null');
-    //     }
-    // };
+    var findMagic = function(cells){
+        var modes = []
+        for(i = 0; i < cells.length; i++){
+            if(cells[i].get_text().startsWith('%%')){
+                console.log('get magic');
+                var magic = cells[i].get_text().split(/ |\n/)[0]; 
+                if (magic.length > 2){
+                    magic = magic.substring(2,magic.length);
+                    if(!modes.includes(magic)) modes.push(magic);
+                }
+            }
+        }
+        return modes;
+    }
     
+    //HardCode SQL for now
     var changeHighlight = function(){
         console.log("in change");
-        codecell.CodeCell.options_default.highlight_modes['magic_text/x-mssql'] = { 'reg': [/^%%sql/] };
+        codecell.CodeCell.options_default.highlight_modes['magic_text/x-mysql'] = { 'reg': [/^%%sql/] };
         Jupyter.notebook.events.one('kernel_ready.Kernel', function () {
             Jupyter.notebook.get_cells().map(function (cell) {
                 if (cell.cell_type == 'code') { cell.auto_highlight(); }
             });
         });
     };
-    var load_ipython_extension = function () {
-        Jupyter.toolbar.add_buttons_group([
-            Jupyter.keyboard_manager.actions.register({
-                'help': 'Increase code font size',
-                'icon': 'fa-search-plus',
-                'handler': function () {
-                    $(document).ready(changeHighlight());
-                }
-            }, 'changeHighlight', 'sql_magiclight')
-        ]);
 
-        console.log("loaded sql magic");
+    var initialize = function(){
+        var cells = Jupyter.notebook.get_cells();
+        var modes = findMagic(cells);
+        console.log("modes:" + modes);
+        //searchForCodeMirrorMode();
+        //if not exist
+        changeHighlight();
+    }
+
+    var load_ipython_extension = function () {
+        if (Jupyter.notebook !== undefined && Jupyter.notebook._fully_loaded) {
+            $(document).ready(initialize());
+        }
+
+        events.on("notebook_loaded.Notebook", initialize(),);
+
+        console.log("loaded magiclight");
 
     };
     return {
