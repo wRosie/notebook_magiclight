691eec90b2d27ae83dc7d585581c566efa930da7
wRosie <43913069+wRosie@users.noreply.github.com>
2020-12-08 18:40:14 -0600

    Fixed bugs in test

--- test/test_install.py
+++ test/test_install.py
@@ -8,26 +8,38 @@ from notebook.nbextensions import (install_nbextension, check_nbextension,
     validate_nbextension, validate_nbextension_python
 )
 
+from notebook.config_manager import BaseJSONConfigManager
+
+
 def test_install_nbextension():
     res = install_nbextension('..')
     assert (res is not None)
 
-def test_check():
+def test_check_installed():
     installed = check_nbextension(['notebook_magiclight'], user=False)
     assert (installed)
 
 
-def check_enable():
-    res = enable_nbextension(section='notebook',
+def test_check_enable():
+    enable_nbextension(section='notebook',
                                require='notebook_magiclight/index')
-    print(res)
+    config_dir = os.path.join(_get_config_dir(user=True), 'nbconfig')
+    cm = BaseJSONConfigManager(config_dir=config_dir)
+    enabled = cm.get('notebook').get('load_extensions',
+                                     {}).get('notebook_magiclight/index',
+                                             False)
+    print('hello')
+    assert enabled
 
-def check_disable():
-    res = disable_nbextension(section='notebook',
+def test_check_disable():
+    test_check_enable()
+    disable_nbextension(section='notebook',
                         require='notebook_magiclight/index')
-    print(res)
+    config_dir = os.path.join(_get_config_dir(user=True), 'nbconfig')
+    cm = BaseJSONConfigManager(config_dir=config_dir)
+    enabled = cm.get('notebook').get('load_extensions', {}).get(u'ƒ', False)
+    assert not enabled
 
-test_install_nbextension()
-test_check()
-#check_disable()
-#check_enable()
+# test_install_nbextension()
+# test_check()
+# check_enable()
