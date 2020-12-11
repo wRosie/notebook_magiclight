a65caa91739e96d9d2f3ff5ef8d934aba63e2496
wRosie <43913069+wRosie@users.noreply.github.com>
2020-12-10 21:24:16 -0600

    added index test

---
+++ test/index_test.js
@@ -0,0 +1,25 @@
+/* testing for index.js */
+
+// require("amd-loader");
+// var searchForCodeMirrorMode = require('../index.js');
+// var expect = require('chai').expect;
+
+// describe('tests for index.js', function () {
+
+//     context('searchForCodeMirrorMode', function () {
+//         it('single element', function () {
+//             expect(searchForCodeMirrorMode(['sql'])['sql']['mime']).to.equal("text/x-sql");
+//         })
+
+//         it('element list 1', function () {
+//             expect(searchForCodeMirrorMode(['sql', 'html'])['sql']['mime']).to.equal("text/x-sql");
+//         })
+
+//         it('element list 2', function () {
+//             expect(searchForCodeMirrorMode(['sql', 'html'])['html']['mime']).to.equal("text/x-sql");
+//         })  
+//     })
+
+
+
+// })
